377699ca3bc78ea4a0f802a2b5eb5e2fbef3bedf
hirschan <alfred@hirschfeld.se>
2023-10-25 21:41:04 +0200

    Refactor css, introduce contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,38 @@
+// Waiting for DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Remove language icon since we cannot modify its colors
+    const langIcon = document.querySelector('.vector-icon.mw-ui-icon-language-progressive.mw-ui-icon-wikimedia-language-progressive');
+    if (langIcon) {
+        langIcon.style.backgroundImage = 'none';
+    }
+
+    // Get all elements on the page
+    const allElements = document.querySelectorAll('*');
+
+    // Iterate through each element and check its computed color
+    allElements.forEach(element => {
+        const computedStyles = window.getComputedStyle(element);
+        const currentColor = computedStyles.color;
+        const currentBackgroundColor = computedStyles.backgroundColor
+
+        // Search button text to white color
+        if (currentColor === 'rgb(32, 33, 34)') {
+            element.style.color = '#c9d1d9'
+        }
+        // Headline text to white color
+        if (currentColor === 'rgb(0, 0, 0)') {
+            element.style.color = '#c9d1d9'
+        }
+
+        // Grey background to lighter dark background
+        if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+            element.style.backgroundColor = '#21262d'
+        }
+        if (currentColor === 'rgb(234, 236, 240)') {
+            element.style.backgroundColor = '#21262d'
+        }
+
+    });
+
+});
